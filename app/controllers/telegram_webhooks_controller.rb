96ddb7a362a1f78e5dcb961aead677cbe8f66e20
Cory Stephenson <aevin@me.com>
2022-04-12 02:17:53 -0500

    Fix registration badge number field.

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -15,15 +15,19 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
         respond_with :message, text: t('.format')
         return
       end
-      badge_id = Integer(sections[1], exception: false)
-      unless badge_id
+      badge_number = Integer(sections[1], exception: false)
+      unless badge_number
         respond_with :message, text: t('.format_bad_badge')
         return
       end
       name = sections[2..].join(' ')
-      if User.create(telegram_user_id: from['id'], telegram_chat_id: chat['id'], name: name, badge_id: badge_id)
-        respond_with :message, text: t('.registration_complete', badge_number: badge_id, name: name)
-      else
+      begin
+        if User.create(telegram_user_id: from['id'], telegram_chat_id: chat['id'], name: name, badge_number: badge_number)
+          respond_with :message, text: t('.registration_complete', badge_number: badge_number, name: name)
+        else
+          respond_with :message, text: t('registration_failed')
+        end
+      rescue
         respond_with :message, text: t('registration_failed')
       end
     end
