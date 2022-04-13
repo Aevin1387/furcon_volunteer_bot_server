de4ce7433a0d12e3481f898dc1db46360f7b1643
Cory Stephenson <aevin@me.com>
2022-04-12 22:25:40 -0500

    Add start and end shift commands and stubs for other commands.

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -36,4 +36,47 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
   def help!(*)
     respond_with :message, text: t('.content')
   end
+
+  def start_shift!(*)
+    logger.info "Running start_shift"
+    unless user = User.find_by(telegram_user_id: from['id'], telegram_chat_id: chat['id'])
+      reply_with :message, text: t('please_register')
+      return
+    end
+    if existing_shift = Shift.where(user_id: user.id, end_time: nil).first
+      reply_with :message, text: t('existing_shift')
+      return
+    end
+    shift = Shift.create(user: user, start_time: Time.now, chat_id: @chat_id)
+    logger.info "Sending message #{user.name} has started a shift"
+    reply_with :message, text: t('shift_started')
+  end
+
+  def end_shift!(*)
+    logger.info "Running end_shift"
+    unless user = User.find_by(telegram_user_id: from['id'], telegram_chat_id: chat['id'])
+      reply_with :message, text: t('please_register')
+      return
+    end
+    unless existing_shift = Shift.where(user_id: user.id, end_time: nil).first
+      reply_with :message, text: t('no_shift')
+      return
+    end
+
+    existing_shift.update(end_time: Time.now)
+    shift_length = TimeDifference.between(shift.start_time, shift.end_time).humanize
+    reply_with :message, text: t('shift_ended', shift_length: shift_length)
+  end
+
+  def list_shifts!(*)
+  end
+
+  def on_shift!(*)
+  end
+
+  def all_hours!(*)
+  end
+
+  def all_shifts_detailed(*)
+  end
 end
