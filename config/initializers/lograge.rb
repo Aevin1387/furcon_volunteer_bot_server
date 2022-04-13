5341e41d7e2aba27d8800a21ab12f63a81adf0e3
Cory Stephenson <aevin@me.com>
2022-04-12 23:37:09 -0500

    Move lograge to initializer.

---
+++ config/initializers/lograge.rb
@@ -0,0 +1,10 @@
+# Configure Lograge
+config.lograge.enabled = true
+config.lograge.base_controller_class = 'ActionController::API'
+config.lograge.formatter = Lograge::Formatters::Json.new
+config.lograge.custom_options = lambda do |event|
+  exceptions = %w(controller action format id)
+  {
+    params: event.payload[:params].except(*exceptions)
+  }
+end
