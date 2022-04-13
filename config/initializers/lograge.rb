16f4d92a7c9e3d25629cca9a9c510eb257684e7c
Cory Stephenson <aevin@me.com>
2022-04-13 00:16:57 -0500

    Another attempt to get logging working.

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Configure Lograge
   config.lograge.enabled = true
-  config.lograge.base_controller_class = ['ActionController::API', 'AbstractController::Base']
+  config.lograge.base_controller_class = ['ActionController::API', 'AbstractController::Base', 'Telegram::Bot::UpdatesController']
   config.lograge.formatter = Lograge::Formatters::Json.new
   config.lograge.custom_options = lambda do |event|
     exceptions = %w(controller action format id)
@@ -9,15 +9,4 @@ Rails.application.configure do
       params: event.payload[:params].except(*exceptions)
     }
   end
-end
-
-app = Rails.application
-if app.config.lograge.enabled
-  ActiveSupport::LogSubscriber.log_subscribers.each do |subscriber|
-    case subscriber
-    when ActionController::LogSubscriber
-      Lograge.unsubscribe('updates_controller.bot.telegram', subscriber)
-    end
-  end
-  Lograge::RequestLogSubscriber.attach_to 'updates_controller.bot.telegram'
 end
